07bbbc2d3545f2ef575c4e8e06f32ba867fabeb9
Luis Cervantes <lufemarc@gmail.com>
2026-03-23 15:08:57 -0500

    Update apiUrl to include '/api' path

--- caudo-frontend/src/environments/environment.prod.ts
+++ caudo-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://caudo-app.livelyplant-21b4c16c.westus2.azurecontainerapps.io',
+    apiUrl: 'https://caudo-app.livelyplant-21b4c16c.westus2.azurecontainerapps.io/api',
     apiKey: 'caudo-api-key-2024'
 };
