3d0e3f13830106938e70d55cc69849ce33607def
Luis Cervantes <lufemarc@gmail.com>
2026-03-23 14:57:06 -0500

    Update environment.prod.ts

--- caudo-frontend/src/environments/environment.prod.ts
+++ caudo-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://caudo-1.onrender.com/api',
+    apiUrl: 'https://caudo-app.livelyplant-21b4c16c.westus2.azurecontainerapps.io',
     apiKey: 'caudo-api-key-2024'
 };
